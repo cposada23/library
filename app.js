615a7c13cdd97f9809a9e82052f0181a0b097857
Camilo Posada Angel <cposadaa@gmail.com>
2020-04-23 18:46:08 -0500

    Serving static files from node_modules

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ app.use(morgan('tiny'))
 
 // Serve static files 
 app.use(express.static(path.join(__dirname, "/public")))
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.htm'))
