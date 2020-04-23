7474b1cbd3de7ec5b3305b442831ce05ff443326
Camilo Posada Angel <cposadaa@gmail.com>
2020-04-23 18:12:52 -0500

    add debug and morgar for better logging and debuggin

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var express = require('express')
+var chalk = require('chalk')
+var debug = require('debug')('app')
+var morgan = require('morgan')
+
 
 var app = express()
+
+app.use(morgan('combined'))
+
 app.get('/', (req, res) => {
      res.send('Hello from my library app')
+})
+
+app.listen(3000, () => {
+    debug(`listening on port ${chalk.green('3000')}`)
 })
