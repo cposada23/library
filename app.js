fdbfb4b485c75106da6d64c6bd600b3d9006152c
Camilo Posada Angel <cposadaa@gmail.com>
2020-04-23 19:36:04 -0500

    eslint, nodemon config

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-var express = require('express')
-var chalk = require('chalk')
-var debug = require('debug')('app')
-var morgan = require('morgan')
-var path = require('path')
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 
-// Serve static files 
-app.use(express.static(path.join(__dirname, "/public")))
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+// Serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.htm'))
-})
+  res.sendFile(path.join(__dirname, 'views/index.htm'));
+});
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.green('3000')}`)
-})
+app.listen(port, () => {
+  debug(`listening on port ${chalk.green(port)}`);
+});
