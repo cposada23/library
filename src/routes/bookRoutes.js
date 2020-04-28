defb02dcb2901322d36798d39cd0f11ecf499c1b
Camilo Posada Angel <cposadaa@gmail.com>
2020-04-27 19:31:58 -0500

    route books

---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const debug = require('debug')('bookRoutes');
+const chalk = require('chalk');
+
+const bookRouter = express.Router();
+
+function router(nav) {
+  const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    },
+    {
+      title: 'Les Misérables',
+      genre: 'Historical Fiction',
+      author: 'Victor Hugo',
+      read: false
+    },
+    {
+      title: 'The Time Machine',
+      genre: 'Science Fiction',
+      author: 'H. G. Wells',
+      read: false
+    },
+    {
+      title: 'A Journey into the Center of the Earth',
+      genre: 'Science Fiction',
+      author: 'Jules Verne',
+      read: false
+    },
+    {
+      title: 'The Dark World',
+      genre: 'Fantasy',
+      author: 'Henry Kuttner',
+      read: false
+    },
+    {
+      title: 'The Wind in the Willows',
+      genre: 'Fantasy',
+      author: 'Kenneth Grahame',
+      read: false
+    },
+    {
+      title: 'Life On The Mississippi',
+      genre: 'History',
+      author: 'Mark Twain',
+      read: false
+    },
+    {
+      title: 'Childhood',
+      genre: 'Biography',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false
+    }
+  ];
+  bookRouter.route('/').get((req, res) => {
+    debug('Get all books');
+    res.render('bookListView', {
+      nav,
+      title: 'Library',
+      books
+    });
+  });
+  bookRouter.route('/:id').get((req, res) => {
+    const { id } = req.params;
+    debug(`BookID: ${chalk.green(id)}`);
+    res.render('bookView', {
+      nav,
+      title: 'Library',
+      book: books[id]
+    });
+  });
+
+  return bookRouter;
+}
+
+module.exports = router;
